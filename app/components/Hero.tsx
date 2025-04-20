d02fe710feab36c41954b2c078bdd72d627d67b9
integral_digi <ojaduagreat@gmail.com>
2025-04-20 12:01:29 +0100

    refactor Hero component for improved code readability

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,76 +2,76 @@
 import { motion } from "framer-motion";
 
 const heroItems = {
-  info: "Software Design & Development Agency Based in Lagos Nigeria",
-  title: "Turning Great Ideas into Exceptional Digital Products",
-  subtitle:
-    "Whether you’re still validating an idea or ready to scale an existing product, our team of designers, developers, and product experts is ready to help you go from vision to version 1, and beyond.",
-  buttonText: "Book a Free Discovery Call",
+	info: "Software Design & Development Agency Based in Lagos Nigeria",
+	title: "Turning Great Ideas into Exceptional Digital Products",
+	subtitle:
+		"Whether you’re still validating an idea or ready to scale an existing product, our team of designers, developers, and product experts is ready to help you go from vision to version 1, and beyond.",
+	buttonText: "Book a Free Discovery Call",
 };
 
 export const style = {
-  paragraph: "text-base text-white font-normal",
+	paragraph: "text-base text-white font-normal",
 };
 
 // Container variant: This will stagger its children.
 const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
+	hidden: { opacity: 0 },
+	visible: {
+		opacity: 1,
+		transition: {
+			delayChildren: 0.3,
+			staggerChildren: 0.2,
+		},
+	},
 };
 
 // Child variant: Each element will fade in and slide upward.
 const childVariants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+	hidden: { opacity: 0, y: 50 },
+	visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
 };
 
 const Hero = () => {
-  return (
-    <div className="w-full flex items-center justify-center px-4 py-12 md:py-24">
-      <motion.div
-        className="space-y-12 flex flex-col justify-center w-full md:w-3/4"
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-      >
-        {/* Info */}
-        <motion.p
-          className={`text-center ${style.paragraph}`}
-          variants={childVariants}
-        >
-          {heroItems.info}
-        </motion.p>
+	return (
+		<div className="w-full flex items-center justify-center px-4 py-12">
+			<motion.div
+				className="space-y-12 flex flex-col justify-center w-full md:w-3/4"
+				variants={containerVariants}
+				initial="hidden"
+				animate="visible"
+			>
+				{/* Info */}
+				<motion.p
+					className={`text-center ${style.paragraph}`}
+					variants={childVariants}
+				>
+					{heroItems.info}
+				</motion.p>
 
-        {/* Title & Subtitle */}
-        <motion.div className="space-y-5" variants={childVariants}>
-          <motion.h1 className="text-white text-center font-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[80px]">
-            {heroItems.title}
-          </motion.h1>
-          <motion.p
-            className={`text-center px-4 sm:px-8 md:px-20 ${style.paragraph}`}
-            variants={childVariants}
-          >
-            {heroItems.subtitle}
-          </motion.p>
-        </motion.div>
+				{/* Title & Subtitle */}
+				<motion.div className="space-y-6" variants={childVariants}>
+					<motion.h1 className="text-white text-center font-black text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[80px]">
+						{heroItems.title}
+					</motion.h1>
+					<motion.p
+						className={`text-center px-4 sm:px-8 md:px-20 ${style.paragraph}`}
+						variants={childVariants}
+					>
+						{heroItems.subtitle}
+					</motion.p>
+				</motion.div>
 
-        {/* Button */}
-        <motion.button
-          className="cursor-pointer px-6 h-16 bg-white rounded-full text-stone-900 font-bold w-fit mx-auto"
-          variants={childVariants}
-          whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
-        >
-          {heroItems.buttonText}
-        </motion.button>
-      </motion.div>
-    </div>
-  );
+				{/* Button */}
+				<motion.button
+					className="cursor-pointer px-6 h-16 bg-white rounded-full text-stone-900 font-bold w-fit mx-auto"
+					variants={childVariants}
+					whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+				>
+					{heroItems.buttonText}
+				</motion.button>
+			</motion.div>
+		</div>
+	);
 };
 
 export default Hero;
